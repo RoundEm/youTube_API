f9c58b2d14af8851ee730ad580ce4e829a4d46cc
Jason Roundtree <jasontree123@gmail.com>
2017-12-14 20:40:57 -0800

    most of js and a few minor changes to html, css

--- index.js
+++ index.js
@@ -1,1 +1,66 @@
 const YOUTUBE_SEARCH_URL = 'https://www.googleapis.com/youtube/v3/search';
+
+funtion getDataFromApi(searchTerm, callback, pageToken) {
+	if (typeof pageToken === 'undefined') {
+		pageToken = '';
+	}
+	const query {
+		part: 'snippet',
+	  	key: 'AIzaSyC66T2dUVOC-aw6kL9JEnOzNDQKJ21grag',
+	    q: `${searchTerm}`,
+	    pageToken: pageToken,
+	    maxResults: 20
+  	}
+  	$.getJSON(YOUTUBE_SEARCH_URL, query, callback);
+}
+
+const youTubeSearch = {
+	currentQuery: '',
+};
+
+function submitListen() {
+	$('.js-search-form').submit(event => {
+		event.preventDefault;
+		const $queryTarget = $($this).find('.js-query');
+		youTubeSearch.currentQuery = queryTarget.val();
+		// clear search field
+		$queryTarget.val() = '';
+		getDataFromApi(youTubeSearch.currentQuery, displayYouTubeSearchData);
+		showMoreResults();
+	});
+}
+
+function displayYouTubeSearchData(data) {
+	const results = data.items.map((item) => renderResult(item));
+	$('js-search-results').html(results);
+	// find token # of next and previous results
+	const nextPageId = data.nextPageToken;
+	const prevPageId = data.prevPageToken;
+	nextPage(nextPageId);
+	prevPage(prevPageId);
+}
+
+
+function renderResult(result) {
+	console.log('Result:', result);
+	return `
+		<div>
+			${result.snippet.title} <br>
+			<a href="https://www.youtube.com/watch?v=${result.id.videoId}" target="_blank"><img src="${result.snippet.thumbnails.default.url}"></a>
+		</div>
+	`;
+}
+
+function showMoreResults() {
+  $('.nextPage').fadeIn(2500);
+}
+
+function nextPage(nextPageId) {
+	$('.nextPage').submit(event => {
+		event.preventDefault();
+		console.log('next page token:', nextPageId);
+		getDataFromApi(youTubeSearch.currentQuery, displayYouTubeSearchData, nextPageId);
+	});
+}
+
+$(submitListen);
